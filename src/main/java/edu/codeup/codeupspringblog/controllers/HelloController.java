dba73e8ab78b57e31277fa6cb614c8fe1632d5d3
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-09-14 15:09:06 -0500

    separated distance and hello maps to different controllers

--- src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/HelloController.java
@@ -3,25 +3,22 @@ package edu.codeup.codeupspringblog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("/hello/world")
+    @GetMapping("")
     @ResponseBody
     public String helloWorld(){
         return "Hello, World!";
     }
-    @GetMapping("/hello/{name}")
+    @GetMapping("/{name}")
     @ResponseBody
     public String helloName(@PathVariable String name){
         return String.format("Hello, %s!", name);
     }
-    @GetMapping("/distance/{num1}/{num2}")
-    @ResponseBody
-    public String distanceBetweenNumbers(@PathVariable int num1, @PathVariable int num2){
-        int distance = Math.abs(num1-num2);
-        return String.format("%s is %s digits away from %s.", num1, distance, num2);
-    }
+
 
 }
