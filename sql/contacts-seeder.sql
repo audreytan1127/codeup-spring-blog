0f6f537058a80b20486607ee2da605f9b47bdd60
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-09-18 12:02:40 -0500

    added contacts and dogs table to blog_db. Added columns and seeder info to both tables. Added Controller, model, and repository (Dao) for Contact.

---
+++ sql/contacts-seeder.sql
@@ -0,0 +1,6 @@
+USE blog_db;
+INSERT INTO contacts (email, name) VALUES
+                                       ('justin@email.com', 'Justin'),
+                                       ('laura@email.com', 'Laura'),
+                                       ('audrey@email.com', 'Audrey'),
+                                       ('steve@email.com', 'Steve');
---
+++ src/main/java/edu/codeup/codeupspringblog/models/BlogPost.java
@@ -0,0 +1,56 @@
+package edu.codeup.codeupspringblog.models;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "blogs")
+public class BlogPost {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(columnDefinition = "int(11) UNSIGNED")
+    private long id;
+    @Column(nullable = false, length = 200)
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    public BlogPost() {
+    }
+
+    public BlogPost(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public BlogPost(long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
+
