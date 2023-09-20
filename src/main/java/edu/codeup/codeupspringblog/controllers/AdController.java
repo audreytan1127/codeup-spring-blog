ec7f2d3c84eeae195c01cc83288ca9951c8f9066
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-09-20 15:04:29 -0500

    completed services class lecture

--- src/main/java/edu/codeup/codeupspringblog/controllers/AdController.java
+++ src/main/java/edu/codeup/codeupspringblog/controllers/AdController.java
@@ -2,6 +2,7 @@ package edu.codeup.codeupspringblog.controllers;
 
 import edu.codeup.codeupspringblog.models.Ad;
 import edu.codeup.codeupspringblog.repositories.AdRepository;
+import edu.codeup.codeupspringblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +13,7 @@ import java.util.List;
 public class AdController {
 
     private AdRepository adsDao;
+    private EmailService emailService;
 
     public AdController(AdRepository adsDao) {
         this.adsDao = adsDao;
@@ -51,6 +53,7 @@ public class AdController {
                 ad.getDescription()
         );
         adsDao.save(adToSave);
+        emailService.prepareAndSend(adToSave, "You created an ad!", "Here is some more info from your ad!");
         return "redirect:/ads";
     }
 
